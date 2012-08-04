d9e3790c6f6160e57c2bcefe5a09fc28d03a29ea
Mathijs Kadijk <mathijs@wrep.nl>
2012-08-04 16:46:36 +0200

    Removed special chars in statusbar and cleaned up the code a bit
    - Removed special chars around the message in the menubar
    - Call IOPSGetTimeRemainingEstimate() only once
    - Added comments
    - Using more properties to improve readability

--- Battery Time Remaining/AppDelegate.h
+++ Battery Time Remaining/AppDelegate.h
@@ -10,7 +10,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-- (NSString*)GetTimeRemainingText;
+- (NSString*)getTimeRemainingText;
 
 @property (assign) IBOutlet NSWindow *window;
 @property (strong) NSStatusItem *statusItem;

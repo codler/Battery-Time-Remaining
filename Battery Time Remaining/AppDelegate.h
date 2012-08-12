39573ee94141ee8f64aa1a3f6961951504e31729
Han Lin Yap <codler@gmail.com>
2012-08-12 14:32:48 +0200

    Notify user when a newer version of the app is available

--- Battery Time Remaining/AppDelegate.h
+++ Battery Time Remaining/AppDelegate.h
@@ -8,13 +8,14 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate, NSMenuDelegate>
 
 - (void)updateStatusItem;
 - (NSImage *)getBatteryIconNamed:(NSString *)iconName;
 
 @property (strong) NSStatusItem *statusItem;
 @property (strong) NSMenuItem *startupToggle;
+@property (strong) NSMenuItem *updaterMenu;
 @property (nonatomic) NSInteger previousPercent;
 @property (strong) NSMutableDictionary *notifications;
 

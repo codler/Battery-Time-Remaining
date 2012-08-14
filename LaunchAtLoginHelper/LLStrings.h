a540686179645abe9f12c817d16c5e075ec095c9
Han Lin Yap <codler@gmail.com>
2012-08-14 14:13:19 +0200

    Sandboxing start at login (#4)

---
+++ LaunchAtLoginHelper/LLStrings.h
@@ -0,0 +1,5 @@
+// strings used by LLManager and LaunchAtLoginHelper
+//
+
+#define LLURLScheme @"com.codler.Battery-Time-Remaining"
+#define LLHelperBundleIdentifier @"com.codler.Battery-Time-Remaining-Helper"

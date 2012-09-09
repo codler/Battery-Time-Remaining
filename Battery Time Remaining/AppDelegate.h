8fe8e94f4d29ac118c29ed6ae57fffdac6712e78
Oliver Sigge <osigge@mac.com>
2012-09-10 00:17:24 +0200

    Cleaned AppDelegate

--- Battery Time Remaining/AppDelegate.h
+++ Battery Time Remaining/AppDelegate.h
@@ -8,32 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-#ifndef _BTR_MENU
-#define _BTR_MENU
-
-#define kBTRMenuPowerSourcePercent  1
-#define kBTRMenuPowerSourceState    2
-#define kBTRMenuPowerSourceAdvanced 3
-#define kBTRMenuStartAtLogin        4
-#define kBTRMenuNotification        5
-#define kBTRMenuSetting             6
-#define kBTRMenuAdvanced            7
-#define kBTRMenuParenthesis         8
-#define kBTRMenuEnergySaverSetting  9
-#define kBTRMenuUpdater             10
-#define kBTRMenuQuitKey             11
-
-#endif
-
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
-//- (void)updateStatusItem;
-//- (NSImage *)getBatteryIconNamed:(NSString *)iconName;
-//- (NSImage *)getBatteryIconPercent:(NSInteger)percent;
-
-@property (nonatomic) NSInteger previousPercent;
-@property (nonatomic) NSInteger currentPercent;
-@property (strong) NSMutableDictionary *notifications;
-@property (nonatomic) bool advancedSupported;
 
 @end
